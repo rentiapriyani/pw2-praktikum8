92fb9c2a4701457fae82a979cca04e8729dca003
rentiapriyani <147421732+rentiapriyani@users.noreply.github.com>
2024-05-29 09:17:56 +0700

    Add files via upload

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function show(): View
+    {
+        return view('dashboard/index');
+    }
+    public function laporan(): View
+    {
+        return view('dashboard/laporan');
+    }
+}
